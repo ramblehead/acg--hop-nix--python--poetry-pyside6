279041a75e7ee339218ae327ab9dd7b62b6d61e1
ramblehead <v.rybynok@gmail.com>
2026-01-21 08:54:36 +0000

    Checkpoint

---
+++ bootstrap/package.json.gen.py
@@ -0,0 +1,32 @@
+# Hey Emacs, this is -*- coding: utf-8 -*-
+
+from string import Template
+from typing import TYPE_CHECKING
+
+from autocodegen.utils import kebab_case
+
+if TYPE_CHECKING:
+    from autocodegen import Context
+
+template_str = """\
+{
+  "name": "${project_name_kebab}",
+  "packageManager": "yarn@4.11.0",
+  "devDependencies": {
+    "prettier": "^3.6.2",
+    "prettier-plugin-pkg": "^0.21.2",
+    "prettier-plugin-sh": "^0.18.0",
+    "prettier-plugin-toml": "^2.0.6"
+  }
+}
+"""
+
+
+def generate(ctx: Context) -> str:
+    project_name = ctx.project_config.autocodegen.project_name
+
+    return Template(template_str).substitute(
+        {
+            "project_name_kebab": kebab_case(project_name),
+        },
+    )
