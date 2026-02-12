0f3e2027a224e0a72b34fe2afb07a797b595c3df
ramblehead <v.rybynok@gmail.com>
2026-02-12 00:07:23 +0000

    Checkpoint

--- bootstrap/.rh-project.ren/init.el.gen.py
+++ bootstrap/.rh-project.ren/init.el.gen.py
@@ -1,3 +1,14 @@
+# Hey Emacs, this is -*- coding: utf-8 -*-
+
+from string import Template
+from typing import TYPE_CHECKING
+
+from autocodegen.utils import kebab_case
+
+if TYPE_CHECKING:
+    from autocodegen import Context
+
+template_str = r"""\
 ;; Hey Emacs, this is -*- coding: utf-8 -*-
 
 (require 'hydra)
@@ -179,3 +190,14 @@
           (run-with-idle-timer 0 nil #'prettier-mode)))))))
 
 ;;; /b/}
+"""
+
+
+def generate(ctx: Context) -> str:
+    project_name = ctx.template_config.project_name
+
+    return Template(template_str).substitute(
+        {
+            "project_name_kebab": kebab_case(project_name),
+        },
+    )
---
+++ bootstrap/.rh-project.ren/setup.el.gen.py
@@ -0,0 +1,26 @@
+# Hey Emacs, this is -*- coding: utf-8 -*-
+
+from string import Template
+from typing import TYPE_CHECKING
+
+from autocodegen.utils import kebab_case
+
+if TYPE_CHECKING:
+    from autocodegen import Context
+
+template_str = r"""\
+;; -*- coding: utf-8 -*-
+
+(acg-templates-mode 1)
+(acg-templates-setup)
+"""
+
+
+def generate(ctx: Context) -> str:
+    project_name = ctx.template_config.project_name
+
+    return Template(template_str).substitute(
+        {
+            "project_name_kebab": kebab_case(project_name),
+        },
+    )
