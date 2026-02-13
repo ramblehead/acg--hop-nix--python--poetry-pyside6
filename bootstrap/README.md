7c7b28c76e8fb50607b51d785c62c0f9c824c1b1
ramblehead <v.rybynok@gmail.com>
2026-02-13 22:07:38 +0000

    Checkpoint

--- bootstrap/README.md
+++
@@ -1,3 +0,0 @@
-# acg--nix-hop--python--poetry-pyside6
-
-ramblehead's Higher Order Project (HOP) for acg-templates
---
+++ bootstrap/README.md.gen1.py
@@ -0,0 +1,25 @@
+# Hey Emacs, this is -*- coding: utf-8 -*-
+
+from string import Template
+from typing import TYPE_CHECKING
+
+from autocodegen.utils import kebab_case
+
+if TYPE_CHECKING:
+    from autocodegen import Context
+
+template_str = r"""\
+# acg--nix-hop--python--poetry-pyside6
+
+ramblehead's Higher Order Project (HOP) for acg-templates
+"""
+
+
+def generate(ctx: Context) -> str:
+    project_name = ctx.template_config.project_name
+
+    return Template(template_str).substitute(
+        {
+            "project_name_kebab": kebab_case(project_name),
+        },
+    )
